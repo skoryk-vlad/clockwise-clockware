f49c44e36f0e739987a2590403ed6ebffcfcf56c
Vlad <skoryk.v.m@gmail.com>
2022-09-02 13:59:49 +0300

    Update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd ./backend && npm run build
+web: ./backend npm run start
--- backend/src/sequelize.ts
+++ backend/src/sequelize.ts
@@ -1,3 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.DB_CONNECT);
+export const sequelize = new Sequelize(process.env.DB_CONNECT, {
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
