530c996ce34f4af5a2499fe27127782e4f382cb5
Vlad <skoryk.v.m@gmail.com>
2022-09-05 09:02:44 +0300

    Add .env for front-end

--- frontend/src/API/Server.js
+++ frontend/src/API/Server.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = 'https://clockclock-back.herokuapp.com';
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
     baseURL: `${API_URL}/api`
