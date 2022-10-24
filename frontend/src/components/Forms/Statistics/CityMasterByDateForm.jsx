d48020abf0958c6b47835542e8e43fd0fd2a94ff
Vlad <skoryk.v.m@gmail.com>
2022-10-24 17:55:04 +0300

    Fix zod dates in frontend schemas

--- frontend/src/components/Forms/Statistics/CityMasterByDateForm.jsx
+++ frontend/src/components/Forms/Statistics/CityMasterByDateForm.jsx
@@ -8,8 +8,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { isAfter } from 'date-fns';
 
 const CityMasterByDateSchema = z.object({
-    dateStart: z.string().regex(/([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))/).optional().or(z.literal('')),
-    dateEnd: z.string().regex(/([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))/).optional().or(z.literal('')),
+    dateStart: z.literal('').or(z.preprocess(value => (typeof value === "string" || value instanceof Date) && new Date(value),
+        z.date())).optional(),
+    dateEnd: z.literal('').or(z.preprocess(value => (typeof value === "string" || value instanceof Date) && new Date(value),
+        z.date())).optional()
 }).refine(value => !value.dateStart || !value.dateEnd || !isAfter(new Date(value.dateStart), new Date(value.dateEnd)));
 
 export const CityMasterByDateForm = ({ filters, onClick }) => {
--- frontend/src/components/Forms/Statistics/OrdersByDatesForm.jsx
+++ frontend/src/components/Forms/Statistics/OrdersByDatesForm.jsx
@@ -10,8 +10,10 @@ import { CityService, MasterService } from '../../../API/Server';
 import { isAfter } from 'date-fns';
 
 const OrdersByDatesSchema = z.object({
-    dateStart: z.string().regex(/([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))/).optional().or(z.literal('')),
-    dateEnd: z.string().regex(/([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))/).optional().or(z.literal('')),
+    dateStart: z.literal('').or(z.preprocess(value => (typeof value === "string" || value instanceof Date) && new Date(value),
+        z.date())).optional(),
+    dateEnd: z.literal('').or(z.preprocess(value => (typeof value === "string" || value instanceof Date) && new Date(value),
+        z.date())).optional(),
     masters: z.array(z.number().int().positive()).optional(),
     cities: z.array(z.number().int().positive()).optional(),
 }).refine(value => !value.dateStart || !value.dateEnd || !isAfter(new Date(value.dateStart), new Date(value.dateEnd)));
