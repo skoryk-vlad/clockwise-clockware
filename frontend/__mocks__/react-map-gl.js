a4e821a38f7682fe8c921e9b8027f15443ff1159
Vlad <skoryk.v.m@gmail.com>
2022-12-01 16:41:16 +0200

    Add mock for react-map-gl

---
+++ frontend/__mocks__/react-map-gl.js
@@ -0,0 +1,14 @@
+const testComponent = () => {
+    return (
+        <span>Mock</span>
+    )
+};
+
+module.exports = {
+    __esModule: true,
+    default: testComponent,
+    GeolocateControl: testComponent,
+    FullscreenControl: testComponent,
+    NavigationControl: testComponent,
+    Marker: testComponent,
+};
